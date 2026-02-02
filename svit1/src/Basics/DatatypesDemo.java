d5fd7333927f87b85ea6b5314abfa3acb657abc7
venu <venu792004@gmail.com>
2026-02-02 16:43:36 +0530

    First commit

--- svit1/src/Basics/DatatypesDemo.java
+++ svit1/src/Basics/DatatypesDemo.java
@@ -4,7 +4,6 @@ public class DatatypesDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	}
+		System.out.println(" Datatypes are Primitive and Non-Primitive");	}
 
 }
